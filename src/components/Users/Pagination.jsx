1c4d5eec3cd0f512062d62ef6dc324447d555f2e
MichaelNeikovych <robbery06091991@gmail.com>
2020-08-25 11:47:24 +0300

    'pagination'

--- src/components/Users/Pagination.jsx
+++ src/components/Users/Pagination.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Pagination.module.css";
 
-const Pagination = (props) => {
-  const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+const Pagination = ({totalUsersCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
+  const pagesCount = Math.ceil(totalUsersCount / pageSize);
   const pages = [];
   for (let i = 1; i <= pagesCount; i++) pages.push(i);
 
+  const portionCount = Math.ceil(pagesCount / portionSize);
+  const [portionNumber, setPortionNumber] = useState(1);
+  const leftPortionNumber = (portionNumber - 1) * portionSize + 1;
+  const rightPortionNumber = portionNumber * portionSize;
+
   return (
-    <React.Fragment>
       <div className={style.pagination}>
-        {pages.map(p => {
-          return <span className={props.currentPage === p && style.selected}
-                       onClick={() => props.onPageChanged(p)}>{p}</span>
+        {<button disabled={portionNumber > 1 ? false : true} onClick={() => setPortionNumber(portionNumber-1)}>prev</button>}
+
+        {pages
+          .filter(p => p >= leftPortionNumber && p<=rightPortionNumber)
+          .map(p => {
+          return <span className={currentPage === p && style.selected}
+                       onClick={() => onPageChanged(p)}>{p}</span>
         })}
+
+        {<button disabled={portionCount > portionNumber ? false : true} onClick={() => setPortionNumber(portionNumber+1)}>next</button>}
       </div>
-    </React.Fragment>
   )
 }
 
